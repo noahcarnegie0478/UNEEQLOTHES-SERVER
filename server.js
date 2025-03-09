39865d157851c24249bb5757a4602a311284eb00
noahcarnegie0478 <noah.carnegie0478@outlook.com>
2025-03-09 20:54:51 +1000

    merge has not finished

--- server.js
+++ server.js
@@ -57,19 +57,27 @@ app.post("/users/register", async (req, res) => {
     console.error(error);
   }
 });
-app.post("/users/login", async (req, res) => {
-  const user = users.find(user => (user.name = req.body.name));
-  if (user == null) {
-    return res.status(400).send(" Cannot find user");
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      res.send("Success");
-    } else {
-      res.send("Not Allow");
-    }
-  } catch (error) {
-    res.status(500).send();
-  }
-});
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })
+);
+// app.post("/users/login", async (req, res) => {
+//   const user = users.find(user => (user.name = req.body.name));
+//   if (user == null) {
+//     return res.status(400).send(" Cannot find user");
+//   }
+//   try {
+//     if (await bcrypt.compare(req.body.password, user.password)) {
+//       res.send("Success");
+//     } else {
+//       res.send("Not Allow");
+//     }
+//   } catch (error) {
+//     res.status(500).send();
+//   }
+// });
 app.listen(3000);
