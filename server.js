3ec6a53163457ed59fa16f4272eb707e40ef53b0
noahcarnegie0478 <noah.carnegie0478@outlook.com>
2025-03-09 17:55:29 +1000

    nothing changed

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ app.get("/users", (req, res) => {
 app.post("/users", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     console.log(salt + " " + hashedPassword);
     const user = { name: req.body.name, password: hashedPassword };
     users.push(user);
     res.status(201).send();
   } catch (error) {
     res.status(500).send();
+    console.error(error);
   }
 });
 app.post("/users/login", async (req, res) => {
@@ -33,6 +34,7 @@ app.post("/users/login", async (req, res) => {
     }
   } catch (error) {
     res.status(500).send();
+    console.error(error);
   }
 });
 app.listen(3000);
