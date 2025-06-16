3ceab300c657fe5ff42797440c31d1add458d3cf
noahcarnegie0478 <noah.carnegie0478@outlook.com>
2025-06-16 20:37:04 +1000

    storing order has been added to database

--- services/stripe.service.js
+++ services/stripe.service.js
@@ -5,13 +5,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.STRIPE_SECRETKEY);
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 const checkoutSession = async (req, res) => {
   try {
     console.log("run");
-    const { item } = req.body;
+    const { item, user } = req.body;
     const session = await stripe.checkout.sessions.create({
       line_items: item.map(itm => ({
         price_data: {
@@ -19,8 +27,12 @@ const checkoutSession = async (req, res) => {
           unit_amount: Math.round(itm.price * 100),
           product_data: {
             name: itm.title,
-
             images: [itm.image_path],
+            metadata: {
+              size: itm.size,
+              color: itm.color,
+              id: itm.id,
+            },
           },
         },
         quantity: itm.quatity,
@@ -28,6 +40,9 @@ const checkoutSession = async (req, res) => {
       mode: "payment",
       success_url: `${process.env.SUCCESS}?success=true`,
       cancel_url: `${process.env.CANCLE}?canceled=true`,
+      metadata: {
+        user_id: user,
+      },
     });
 
     res.json({ url: session.url, id: session.id });
@@ -36,14 +51,61 @@ const checkoutSession = async (req, res) => {
   }
 };
 
-const listenToService = (request, response) => {
+const listenToService = async (request, response) => {
   const event = request.body;
   // Handle the event
+
+  // if (endpointSecret) {
+  //   // Get the signature sent by Stripe
+  //   const signature = request.headers["stripe-signature"];
+  //   try {
+  //     event = stripe.webhooks.constructEvent(
+  //       request.body,
+  //       signature,
+  //       endpointSecret
+  //     );
+  //   } catch (err) {
+  //     console.log(`⚠️  Webhook signature verification failed.`, err.message);
+  //     return response.sendStatus(400);
+  //   }
+  // }
   switch (event.type) {
     case "payment_intent.succeeded":
       const paymentIntent = event.data.object;
       console.log("payment succeed made: ", paymentIntent);
       console.log("success");
+
+      break;
+    case "checkout.session.completed":
+      const session = event.data.object;
+      const { user_id } = session.metadata;
+      console.log("This is user id:", user_id);
+      const lineItemsResp = await stripe.checkout.sessions.listLineItems(
+        session.id,
+        {
+          expand: ["data.price.product"],
+          limit: 100,
+        }
+      );
+      if (lineItemsResp.data) {
+        for (const item of lineItemsResp.data) {
+          const productData = item.price.product;
+          const { size, color, id } = productData.metadata;
+          const now = new Date();
+          console.log(now);
+          try {
+            const result = await pool.query(
+              "INSERT INTO orders (order_id,customer_id, order_date, item_id, color, size ) VALUES ($1,$2, $3, $4, $5, $6 )",
+              [session.id, user_id, now, id, color, size]
+            );
+            if (result) {
+              console.log("Successfully added!");
+            }
+          } catch (error) {
+            throw error;
+          }
+        }
+      }
 
       break;
     case "payment_method.attached":
